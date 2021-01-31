782754a385df333e9e617eb20dfe868d7226296b
dheimoz <382153+dheimoz@users.noreply.github.com>
2021-01-31 10:53:52 -0600

    210131- Se agrega Sencillo

---
+++ src/components/Sencillo.vue
@@ -0,0 +1,18 @@
+ <template>
+  <div>
+    <h1>Test 1</h1>
+    <h1>Test 2</h1>
+    <h1>Test 3</h1>
+  </div>
+</template>
+ 
+ <script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+ 
+ <style lang="scss" scoped>
+</style>
